89b4f32018f454e49d9c7a9547e8cc76283c3cbf
mitoa89 <mitoa89@gmail.com>
2026-04-19 22:02:45 +0900

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, key, data= None):
+        self.key = key
+        self.data = data
+        self.children = {}
+
+class PrefixTree:
+
+    def __init__(self):
+        self.head = Node(None)
+        
+
+    def insert(self, word: str) -> None:
+        curr = self.head
+        
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = Node(char)
+            curr = curr.children[char]
+        
+        curr.data = word
+
+
+    def search(self, word: str) -> bool:
+        curr = self.head
+
+        for char in word:
+            if char in curr.children:
+                curr = curr.children[char]
+            else:
+                return False
+
+        if curr.data is not None:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.head
+
+        for char in prefix:
+            if char in curr.children:
+                curr = curr.children[char]
+            else:
+                return False
+
+        return True
+        
